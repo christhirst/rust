7209d4311738a83847690fec0dc77de86206c990
chris <christhirst@gmail.com>
2023-09-10 21:18:18 +0200

    start working

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,12 +1,10 @@
-fn main() {
-    use std::fs::File;
-    use std::io::{self, Read};
+fn main() {}
 
-    fn read_username_from_file() -> Result<String, io::Error> {
-        let mut username = String::new();
-
-        File::open("hello.txt")?.read_to_string(&mut username)?;
-
-        Ok(username)
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
     }
 }
